2e4ba8bc1acc7501a9bd546267c5f0de247ae882
LarysaS <artnouvo.corpicorn@gmail.com>
2026-05-30 20:54:15 +0300

    sixth

--- app/notes/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/notes/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function NotesPage() {
-  // Автоматичний перехід на сторінку з паралельними маршрутами та фільтром
-  redirect('/notes/filter/all');
-}
